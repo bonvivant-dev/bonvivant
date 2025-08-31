ea6a2604283b6d51415b8199b7bff4c8c2b85cd3
gouz7514 <gouz7514@gmail.com>
2025-08-31 20:10:39 +0900

    Feat : 앱 기본 레이아웃 (홈, 내서재)

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs>
+      <Tabs.Screen 
+        name="index" 
+        options={{
+          title: '홈',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? 'home' : 'home-outline'} 
+              size={24} 
+              color={color} 
+            />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="library" 
+        options={{
+          title: '내서재',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons 
+              name={focused ? 'library' : 'library-outline'} 
+              size={24} 
+              color={color} 
+            />
+          ),
+        }} 
+      />
+    </Tabs>
+  );
 }
--- app/app/library.tsx
+++ app/app/library.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Library() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>홈</Text>
+      <Text>내서재</Text>
     </View>
   );
 }
